49d8d84bbde328f7d14a8e23f967b0e09e52233c
George Araujo <george.gcac@gmail.com>
2022-12-05 19:09:49 -0300

    Added download of nerd fonts and starship config to install_themes
    Signed-off-by: George Araujo <george.gcac@gmail.com>

--- install_themes.sh
+++ install_themes.sh
@@ -6,6 +6,12 @@
 ADD_APT_REPO="sudo add-apt-repository -y"
 APT_INSTALL="sudo apt install -y"
 GSET="gsettings set"
+USR_DL="$HOME/Downloads"
+USR_FONTS="$HOME/.fonts"
+
+
+## Auxiliary functions
+. install_functions.sh
 
 
 ## Adding repos
@@ -24,6 +30,21 @@ $APT_INSTALL lua5.4 # needed by conky scripts
 $APT_INSTALL variety # wallpaper rotator
 
 
+## Download Nerd Fonts for usage with Starship
+mkdir -p $USR_FONTS
+mkdir -p $USR_DL
+
+site=$(get_latest_github_release_no_v "ryanoasis/nerd-fonts" "Ubuntu.zip")
+aria2c -c --summary-interval 0 -d $USR_DL $site
+unzip -u $USR_DL/Ubuntu.zip -d $USR_FONTS
+rm $USR_DL/Ubuntu.zip
+
+site=$(get_latest_github_release_no_v "ryanoasis/nerd-fonts" "UbuntuMono.zip")
+aria2c -c --summary-interval 0 -d $USR_DL $site
+unzip -u $USR_DL/UbuntuMono.zip -d $USR_FONTS
+rm $USR_DL/UbuntuMono.zip
+
+
 ## Configure Gnome3 settings
 # https://help.gnome.org/users/gnome-help/stable/shell-keyboard-shortcuts.html.en
 $GSET org.gnome.desktop.interface clock-show-date true
@@ -36,5 +57,7 @@ $GSET org.gnome.settings-daemon.plugins.color night-light-enabled true
 $GSET org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 
 
-## Install Starship prompt
+## Install and configure Starship prompt
 curl -sS https://starship.rs/install.sh | sh -s -- -b ~/.local/bin
+cp starship.toml $HOME/.config/
+$GSET org.gnome.desktop.interface monospace-font-name 'UbuntuMono Nerd Font Mono 12'
