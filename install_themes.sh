1a1fa67121bad4e1e1b295910f6b145618e81b7e
George Araujo <george.gcac@gmail.com>
2023-09-03 16:35:20 -0300

    Added gitk dracula theme
    Signed-off-by: George Araujo <george.gcac@gmail.com>

--- install_themes.sh
+++ install_themes.sh
@@ -45,14 +45,28 @@ unzip -u $USR_DL/UbuntuMono.zip -d $USR_FONTS
 rm $USR_DL/UbuntuMono.zip
 
 
+## Themes for softwares
+# gitk dracula theme
+aria2c -c --summary-interval 0 -d $USR_DL https://github.com/dracula/gitk/archive/master.zip
+unzip -u $USR_DL/gitk-master.zip
+cp ~/.config/git/gitk ~/.config/git/gitk_bak
+cp $USR_DL/gitk-master/gitk ~/.config/git/
+rm $USR_DL/gitk-master.zip
+rm -rf $USR_DL/gitk-master/
+
+
 ## Configure Gnome3 settings
 # https://help.gnome.org/users/gnome-help/stable/shell-keyboard-shortcuts.html.en
+ICON_THEME="Yaru-blue-dark"
+THEME="Yaru-blue-dark"
+
 $GSET org.gnome.desktop.interface clock-show-date true
 $GSET org.gnome.desktop.interface clock-show-weekday true
 $GSET org.gnome.desktop.interface color-scheme 'prefer-dark'
-$GSET org.gnome.desktop.interface gtk-theme 'Yaru-blue-dark'
-$GSET org.gnome.desktop.peripherals.mouse natural-scroll true
+$GSET org.gnome.desktop.interface gtk-theme $THEME
+$GSET org.gnome.desktop.interface icon-theme $ICON_THEME
 $GSET org.gnome.desktop.interface show-battery-percentage true
+$GSET org.gnome.desktop.peripherals.mouse natural-scroll true
 $GSET org.gnome.settings-daemon.plugins.color night-light-enabled true
 $GSET org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 
@@ -61,3 +75,4 @@ $GSET org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 curl -sS https://starship.rs/install.sh | sh -s -- -y -b ~/.local/bin
 cp starship.toml $HOME/.config/
 $GSET org.gnome.desktop.interface monospace-font-name 'UbuntuMono Nerd Font Mono 12'
+
