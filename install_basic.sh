80743e7edc79a2d3d59f5b4e0ad82857aa09b414
George Araujo <george.gcac@gmail.com>
2023-12-23 13:54:27 -0300

    Added waveterm
    Signed-off-by: George Araujo <george.gcac@gmail.com>

--- install_basic.sh
+++ install_basic.sh
@@ -2,7 +2,8 @@
 # Created by: George Araújo (george.gcac@gmail.com)
 # Currently for Ubuntu 23.04 (Lunar Lobster)
 
-##### Install commands to use #####
+# region Install commands to use
+
 ADD_APT_REPO="sudo add-apt-repository -y"
 ADD_PUBLIC_KEY="sudo tee /usr/share/keyrings"
 APT_INSTALL="sudo apt install -y"
@@ -17,26 +18,39 @@ GSET="gsettings set"
 PIP_INSTALL="python3 -m pip install --upgrade"
 PIPX_INSTALL="pipx install"
 SNAP_INSTALL="sudo snap install"
+SOFTWARE_DIR="$HOME/Software"
 USR_BIN="$HOME/bin"
 
+# endregion
 
-##### Auxiliary functions #####
+
+# region Auxiliary functions
+
 . install_functions.sh
 
+# endregion
 
-##### Create templates for common file types in right click context menu in nautilus #####
+
+# region Create templates for common file types in right click context menu in nautilus
+
 touch ~/Templates/Empty\ Document
 echo -e '#!/bin/bash\n' > ~/Templates/Empty\ Bash\ Script.sh
 echo -e '#!/usr/bin/python3\n# -*- coding: utf-8 -*-\n' > ~/Templates/Empty\ Python\ Script.py
 touch ~/Templates/Empty\ Python\ File.py
 
+# endregion
 
-##### Copy bin/ folder to home and make the scripts executable #####
+
+# region Copy bin/ folder to home and make the scripts executable
+
 cp -r bin $HOME
 chmod +x $HOME/bin/*
 
+# endregion
 
-##### Adding repos #####
+
+# region Adding repos
+
 # brave browser
 $GET_PUBLIC_KEY https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg | sudo tee /usr/share/keyrings/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" |\
@@ -69,8 +83,11 @@ $ADD_APT_REPO ppa:gerardpuig/ppa
 # $GET_PUBLIC_KEY http://repo.vivaldi.com/stable/linux_signing_key.pub | $ADD_PUBLIC_KEY
 # $ADD_APT_REPO "deb [arch=i386,amd64] http://repo.vivaldi.com/stable/deb/ stable main"
 
+# endregion
 
-##### Installs via apt #####
+
+# region Installs via apt
+
 sudo apt update -y
 $APT_INSTALL aria2  # download manager
 $APT_INSTALL autossh  # automatically retry ssh connection
@@ -136,8 +153,11 @@ $APT_INSTALL ubuntu-cleaner
 # Vivaldi Web Browser
 # $APT_INSTALL vivaldi-stable
 
+# endregion
 
-##### Install via dpkg #####
+
+# region Install via dpkg
+
 # bottom - yet another cross-platform graphical process/system monitor
 site=$(get_latest_github_release "ClementTsang/bottom" "bottom_" "_amd64.deb")
 $DOWNLOAD_FILE $site; $DPKG_INSTALL $DEFAULT_DEB_NAME; rm $DEFAULT_DEB_NAME
@@ -161,8 +181,11 @@ $DOWNLOAD_FILE $site; $DPKG_INSTALL $DEFAULT_DEB_NAME; rm $DEFAULT_DEB_NAME
 $ADD_APT_REPO universe
 $APT_INSTALL libfuse2
 
+# endregion
 
-##### Install downloading binary file #####
+
+# region Install downloading binary file
+
 # tealdeer - a very fast implementation of tldr in Rust
 site=$(get_latest_github_release_no_v "dbrgn/tealdeer" "tealdeer-linux-x86_64-musl")
 aria2c -c --summary-interval 0 -d $USR_BIN $site
@@ -180,8 +203,19 @@ sudo mv completions_bash /usr/share/bash-completion/completions/tldr
 # aria2c -c --summary-interval 0 -d $USR_BIN $site
 # chmod +x $USR_BIN/up
 
+# waveterm - An open-source, cross-platform terminal for seamless workflows
+site=$(get_latest_github_release "wavetermdev/waveterm" "waveterm-linux-x64-v" ".zip")
+aria2c -c --summary-interval 0 -d $SOFTWARE_DIR -o waveterm.zip $site
+cd $SOFTWARE_DIR
+unzip waveterm.zip
+rm waveterm.zip
+cd -
 
-##### Install via installation script #####
+# endregion
+
+
+# region Install via installation script
+
 # Dua - view disk space usage and delete unwanted data, fast
 curl -LSfs https://raw.githubusercontent.com/byron/dua-cli/master/ci/install.sh | \
     sh -s -- --git byron/dua-cli --target x86_64-unknown-linux-musl --crate dua
@@ -197,8 +231,11 @@ nvm install $(nvm ls-remote | grep -i latest | tail -n 1 | sed -ne 's/[^v0-9]*\(
 # Poetry Python Dependency Manager
 curl -sSL https://install.python-poetry.org | python3 -
 
+# endregion
 
-##### Installs via snaps #####
+
+# region Installs via snaps
+
 $SNAP_INSTALL code --classic  # vscode
 $SNAP_INSTALL htop
 $SNAP_INSTALL indicator-sound-switcher
@@ -212,8 +249,11 @@ $SNAP_INSTALL telegram-desktop
 $SNAP_INSTALL vlc
 sudo snap refresh
 
+# endregion
 
-##### Install via pip #####
+
+# region Install via pip
+
 # $PIP_INSTALL colorama  # colorful print
 $PIP_INSTALL ipython
 # $PIP_INSTALL ipdb
@@ -227,18 +267,27 @@ $PIPX_INSTALL telegram-send
 $PIPX_INSTALL thefuck  # corrects your previous console command
 $PIP_INSTALL tqdm
 
+# endregion
 
-##### Install via cargo #####
+
+# region Install via cargo
+
 # $CARGO_INSTALL --locked navi  # an interactive cheatsheet tool for the command-line
 # navi repo add denisidoro/cheats
 
+# endregion
 
-##### Upgrade packages and remove unecessary ones #####
+
+# region Upgrade packages and remove unecessary ones
+
 sudo apt -y upgrade
 sudo apt -y autoremove
 
+# endregion
 
-##### Configure flameshot as the default action for PrtScn key #####
+
+# region Configure flameshot as the default action for PrtScn key
+
 # https://askubuntu.com/questions/1036473/ubuntu-18-how-to-change-screenshot-application-to-flameshot
 # Release the PrtScr binding
 # $GSET org.gnome.settings-daemon.plugins.media-keys screenshot '[]'
@@ -254,16 +303,25 @@ sudo apt -y autoremove
 # Fix bug when using cedilla in skype and slack
 # sudo sh -c 'echo GTK_IM_MODULE=cedilla >> /etc/environment'
 
+# endregion
 
-##### Configure git #####
+
+# region Configure git
+
 bash config_git.sh
 
+# endregion
 
-##### Configure themes #####
+
+# region Configure themes
+
 bash install_themes.sh
 
+# endregion
 
-##### Install Ruby (needed for jekyll sites) #####
+
+# region Install Ruby (needed for jekyll sites)
+
 $APT_INSTALL libssl-dev libyaml-dev
 $GIT_CLONE https://github.com/rbenv/rbenv.git $HOME/.rbenv
 . ~/.bashrc
@@ -271,3 +329,4 @@ mkdir -p "$(rbenv root)"/plugins
 $GIT_CLONE https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 rbenv install $(rbenv install -l | grep -v - | tail -1)
 
+# endregion
